01077b77027656e6d3b6b245738b83c5d1cde039
Sarayu-chityala <sarayuchityala15@gmail.com>
2025-03-25 14:49:08 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+class Demo2{
+    public static void main(String args[]) {
+    int a=5,b=5;
+    
+        System.out.println("add"+a+b);
+        //System.out.println("sub"+a-b);
+        System.out.println("mul"+a*b);
+        System.out.println("div"+a/b);
+    }
+}
